9b8878c951db248893072ba216ab2db49366d6e5
mohammadhoseein <mohammadhoseeinnemati1384@gmail.com>
2023-10-28 22:52:56 -0700

    first commit

---
+++ python.py
@@ -0,0 +1,12 @@
+#operation
+
+namber_1 = (1+2)
+namber_2 =(5-4)
+namber_3 = (2*4)
+namber_4 = (6/3)
+namber_5 = (3**2)
+
+add = namber_1 , namber_2 , namber_3 , namber_4 , namber_5
+print(add)
+
+print(((1+3) * (9-2) / 2) ** 2)
